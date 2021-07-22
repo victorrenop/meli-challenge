c254df7f37272d94beb8df15a6b6948929c27196
Victor <victorrenop16@gmail.com>
2021-07-22 00:45:37 -0300

    Fixed setup package name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as requirements_file:
     requirements = [line for line in requirements_file]
 
 setup(
-    name="meli-challenge",
+    name="meli_challenge",
     version=__version__,
     author="Victor Poglioni",
     python_requires=">=3.8.5",
