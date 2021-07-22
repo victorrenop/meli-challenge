6f2def771cd05b89f9db207dcf72fa29424f6c14
Victor <victorrenop16@gmail.com>
2021-07-21 22:55:08 -0300

    Changed how the object schema was being checked, fixing false positives when less columns were being passed

--- meli_challenge/utils/common_functions.py
+++ meli_challenge/utils/common_functions.py
@@ -10,7 +10,10 @@ def schema_file_to_schema_object(schema_file: str) -> StructType:
 def check_json_object_schema(json_object: dict, schema_file: str) -> bool:
     with open(schema_file, "r") as f:
         schema = json.load(f)
-        for key, val in json_object.items():
-            if key not in schema or type(val).__name__ != schema[key]:
+        for key, key_type in schema.items():
+            if (
+                key not in json_object
+                or str(key_type) != type(json_object[key]).__name__  # noqa E721
+            ):
                 return False
     return True
